7520fe60b62fe78eed323426c71836d156476368
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-11-05 11:49:27 +0100

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import './App.css'
-import Card from './components/Card'
-import * as React from 'react'
+import './App.css';
+import Card from './components/Card';
+import * as React from 'react';
 
 function App() {
-
   return (
     <>
-      <div className="Header"> HTL Dornbirn 5bWI </div>
-
+      <div className="Header">HTL Dornbirn 5bWI</div>
       <div className="Cards">
-      <Card />
-      <Card />
-      
-      <Card />
-      <Card />
-      
-
-  
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,8 +8,9 @@ interface Props {
 export default function Card({ }: Props): ReactElement {
     return (
         <div className="card"> 
-            <img className="img"src="https://picsum.photos/200/200" alt="" />
-            <div>Lorem ipsum dolor sit amet, consetetur </div>
+            <img className="img"src="https://picsum.photos/200/300" alt="" />
+            <div className="name">Name</div>
+            <div className="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  </div>
         </div>
 
     )
