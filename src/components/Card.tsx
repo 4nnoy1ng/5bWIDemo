30dee355f4f4bd3f671272410fe7faa8d716855a
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-11-19 10:43:26 +0100

    Tailwind hinzugefgt

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Card({ }: Props): ReactElement {
     return (
-        <div className="card"> 
+        <div className="bg-green-500"> 
             <img className="img"src="https://picsum.photos/200/300" alt="" />
             <div className="name">Name</div>
             <div className="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  </div>
