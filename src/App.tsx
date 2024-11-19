5557078fdbd7c2a8708129dcf9ac51c5c98b2e2b
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-11-19 10:50:00 +0100

    Tailwind first design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css';
 import Card from './components/Card';
 import React from 'react';
 
 function App() {
   return (
     <>
-      <div className="Header">HTL Dornbirn 5bWI</div>
+      <div className="bg-yellow-900">HTL Dornbirn 5bWI</div>
       <div className="Cards">
         <Card />
         <Card />
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react'
-import './Card.css'
 
 interface Props {
 
@@ -7,7 +6,7 @@ interface Props {
 
 export default function Card({ }: Props): ReactElement {
     return (
-        <div className="bg-green-500"> 
+        <div className="bg-green-500 w-full sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3"> 
             <img className="img"src="https://picsum.photos/200/300" alt="" />
             <div className="name">Name</div>
             <div className="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  </div>
